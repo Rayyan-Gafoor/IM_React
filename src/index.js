25fc0473ee9f7ac3832d8a1f4ca805da9f6911bd
MemoryGhost26 <60914125+Rayyan-Gafoor@users.noreply.github.com>
2022-04-21 14:10:36 +0200

    hash route used

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import "./index.css";
 
 
 
 ReactDOM.render(
-<BrowserRouter>
+<HashRouter>
 <App/>
-</BrowserRouter>, 
+</HashRouter>, 
 document.getElementById("root")
 
 );

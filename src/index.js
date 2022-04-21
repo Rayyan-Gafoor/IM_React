a3f3340da20315bedba74fc169ba20d8daeb3a5d
Rayyan-Gafoor <60914125+Rayyan-Gafoor@users.noreply.github.com>
2022-04-21 13:19:24 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import "./index.css";
+
+
+
+ReactDOM.render(
+<BrowserRouter>
+<App/>
+</BrowserRouter>, 
+document.getElementById("root")
+
+);
